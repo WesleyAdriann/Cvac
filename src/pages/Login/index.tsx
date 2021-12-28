896be353b53ee48032063682a116d87e24bc46dd
Wesley Adriann <wesleyadriann@gmail.com>
2021-12-28 10:42:54 -0300

    Adds login with email and password
    Signed-off-by: Wesley Adriann <wesleyadriann@gmail.com>

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,14 +1,24 @@
-import React from 'react'
+import React, { useMemo } from 'react'
+import auth from '@react-native-firebase/auth'
+import { ReactNativeFirebase } from '@react-native-firebase/app'
 
 import { LoginTemplate } from '../../atomic/templates'
+import { ILoginFormInputs } from '../../atomic/organisms'
 
 export const Login: React.FC = () => {
-  const handleSubmitLoginEmail = () => {
+  const authentication = useMemo(auth, [])
 
+  const handleSubmitLoginEmail = async (form: ILoginFormInputs) => {
+    try {
+      await authentication.signInWithEmailAndPassword(form.email, form.password)
+    } catch (_error) {
+      const error = _error as ReactNativeFirebase.NativeFirebaseError
+      console.log('error', error.code)
+    }
   }
 
   const handleSubmitLoginSocial = (social: string) => {
-
+    //
   }
 
   return (
