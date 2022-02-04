b5656162485f1d0300cdf2f7525106bd0c3817e3
Wesley Adriann <wesleyadriann@gmail.com>
2022-02-03 21:01:37 -0300

    Fixes VaccineDetails
    Signed-off-by: Wesley Adriann <wesleyadriann@gmail.com>

--- src/atomic/templates/VaccineDetailsTemplate/index.tsx
+++ src/atomic/templates/VaccineDetailsTemplate/index.tsx
@@ -9,78 +9,22 @@ export interface IVaccineDetailsTemplate {
 
 export const VaccineDetailsTemplate: React.FC<IVaccineDetailsTemplate> = ({
   testID = 'VaccineDetails',
-
   ...props
 }) => {
   return (
-    <AppPage testID={testID} {...props} scroll padding={16}>
-      <Text align='center' size={30}>
+    <AppPage testID={testID} {...props} padding={16}>
+      <Flex flex={0.5} justifyContent='center'>
+        <Text align='center' size={30}>
           Vacina X
-      </Text>
-      <Flex marginStyle={20}>
-        <Flex flexDirection='row'>
-          <Flex>
-            <Text>
-                Idade:
-            </Text>
-          </Flex>
-          <Flex marginStart={10}>
-            <Text>
-                ao nascer
-            </Text>
-          </Flex>
-        </Flex>
-
-        <Flex flexDirection='row'>
-          <Flex>
-            <Text>
-                Dose:
-            </Text>
-          </Flex>
-          <Flex marginStart={10}>
-            <Text>
-                única
-            </Text>
-          </Flex>
-        </Flex>
-
-        <Flex flexDirection='row'>
-          <Flex>
-            <Text>
-                Reações adversas:
-            </Text>
-          </Flex>
-          <Flex marginStart={11}>
-            <Text>
-                Y
-            </Text>
-          </Flex>
-        </Flex>
-
-        <Flex flexDirection='row'>
-          <Flex>
-            <Text>
-                Contraindicação:
-            </Text>
-          </Flex>
-          <Flex marginStart={11}>
-            <Text>
-                X
-            </Text>
-          </Flex>
-        </Flex>
-
-        <Flex flexDirection='row'>
-          <Flex>
-            <Text>
-                Prevenção:
-            </Text>
-          </Flex>
-          <Flex marginStart={11}>
-            <Text>
-                Z
-            </Text>
-          </Flex>
+        </Text>
+      </Flex>
+      <Flex flex={2} scroll>
+        <Flex>
+          <Text>Idade: ao nascer</Text>
+          <Text>Dose:  única</Text>
+          <Text>Reações adversas: Y</Text>
+          <Text>Contra indicação: X</Text>
+          <Text>Prevenção: Z</Text>
         </Flex>
       </Flex>
     </AppPage>
--- src/pages/Calendars/index.tsx
+++ src/pages/Calendars/index.tsx
@@ -1,40 +1,38 @@
 import React, { useEffect } from 'react'
 
-import firestore from '@react-native-firebase/firestore'
+// import firestore from '@react-native-firebase/firestore'
 
 import { CalendarsTemplate } from '../../atomic'
 import { NativeStackHeaderProps } from '@react-navigation/native-stack'
 
 export const Calendars: React.FC<NativeStackHeaderProps> = ({ navigation }) => {
   const getCalendars = async () => {
-    const calendars = await firestore().collection('calendar')
+    // const calendars = await firestore().collection('calendar')
     // console.log('"""',calendars)
     // console.log(await calendars.docs[0].id)
-    const vaccines = await firestore().collection('vaccine').get()
+    // const vaccines = await firestore().collection('vaccine').get()
 
-    const fullData: any[] = []
-    vaccines.docs.forEach((doc) => {
-      // calendars.docs.map(foo => console.log('>>', foo))
-      doc.get('calendars').map(async (ca) => console.log('>>', ca.id.id, await calendars.doc(ca.id.id).get()))
+    // const fullData: any[] = []
+    // vaccines.docs.forEach((doc) => {
+    // calendars.docs.map(foo => console.log('>>', foo))
+    // doc.get('calendars').map(async (ca) => console.log('>>', ca.id.id, await calendars.doc(ca.id.id).get()))
 
-      // await calendars.doc(ca.id.path).get()
-    })
-    console.log(fullData)
+    // await calendars.doc(ca.id.path).get()
+    // })
+    // console.log(fullData)
   }
 
   useEffect(() => {
     getCalendars()
   }, [])
 
-  const isTouch = (item: boolean) => {
-    if (item === true) {
-      return navigation.push('vaccineDetails')
-    }
+  const onPress = () => {
+    navigation.push('vaccineDetails')
   }
 
   return (
     <CalendarsTemplate
-      checkIsTouch={isTouch}
+      onPress={onPress}
     />
   )
 }
