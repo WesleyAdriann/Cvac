972ef18b4f7d2a6d207fcf40cee807943e9a3031
brunoteodoromota <45887875+brunoteodoromota@users.noreply.github.com>
2022-02-03 21:16:15 -0300

    Adjustment in VaccineCertificateCategory file

--- src/pages/VaccineCertificateCategory/index.tsx
+++ src/pages/VaccineCertificateCategory/index.tsx
@@ -1,11 +1,9 @@
+import { NativeStackHeaderProps } from '@react-navigation/native-stack'
 import React from 'react'
 
 import { VaccineCertificateCategoryTemplate } from '../../atomic'
 
-export const VaccineCertificateCategory: React.FC = () => {
-  const onPress = () => {
-
-  }
-
+export const VaccineCertificateCategory: React.FC<NativeStackHeaderProps> = ({navigation}) => {
+  const onPress = () => navigation.push('vaccineCertificate')
   return <VaccineCertificateCategoryTemplate onPress={onPress}/>
 }
