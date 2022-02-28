8315b6cbedeab9995195ada21e84e4a0b3910039
Wesley Adriann <wesleyadriann@gmail.com>
2022-02-27 21:53:59 -0300

    Adds types to firestore
    Signed-off-by: Wesley Adriann <wesleyadriann@gmail.com>

--- src/services/firebase/firestore.ts
+++ src/services/firebase/firestore.ts
@@ -1,5 +1,7 @@
 import firestore from '@react-native-firebase/firestore'
 
+import * as types from './firestore.types'
+
 export const collections = {
   calendar: 'calendar',
   dependentVaccineDoc: 'dependentVaccineDoc',
@@ -10,8 +12,8 @@ export const collections = {
 
 const fire = firestore()
 
-export const colletionCalendar = fire.collection(collections.calendar)
-export const colletionDependentVaccineDoc = fire.collection(collections.dependentVaccineDoc)
-export const collectionDependents = fire.collection(collections.dependents)
-export const collectionUsers = fire.collection(collections.users)
-export const collectionVaccine = fire.collection(collections.vaccine)
+export const colletionCalendar = fire.collection<types.ICollectionCalendar>(collections.calendar)
+export const colletionDependentVaccineDoc = fire.collection<types.ICollectionDependentVaccineDoc>(collections.dependentVaccineDoc)
+export const collectionDependents = fire.collection<types.ICollectionDependents>(collections.dependents)
+export const collectionUsers = fire.collection<types.ICollectionUsers>(collections.users)
+export const collectionVaccine = fire.collection<types.ICollectionVaccine>(collections.vaccine)
--- src/services/firebase/firestore.types.ts
+++ src/services/firebase/firestore.types.ts
@@ -10,4 +10,31 @@ export interface ICollectionCalendar {
 
 export interface ICollectionDependentVaccineDoc {
   calendarId: FirebaseFirestoreTypes.DocumentReference
+  dependentId: FirebaseFirestoreTypes.DocumentReference
+  vaccineId: FirebaseFirestoreTypes.DocumentReference
+  doses: number
+}
+
+export interface ICollectionDependents {
+  name: string
+  birthDate: FirebaseFirestoreTypes.Timestamp
+  userUid: FirebaseFirestoreTypes.DocumentReference
+}
+
+export interface ICollectionUsers {
+  name: string
+  uid: string
+}
+
+export interface ICollectionVaccineCalendar {
+  id: FirebaseFirestoreTypes.DocumentReference
+  loop?: number
+  when: number[]
+}
+
+export interface ICollectionVaccine {
+  calendars: ICollectionVaccineCalendar[]
+  disease: string
+  name: string
+  notes: string[]
 }
